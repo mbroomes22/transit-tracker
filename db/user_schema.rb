4042cce6d19d441ff13767645b00ea31e81466e0
mbroomes22 <michelle_broomes@yahoo.com>
2025-03-11 11:29:34 -0400

    created user & trip schemas

---
+++ db/user_schema.rb
@@ -0,0 +1,14 @@
+ActiveRecord::Schema[7.1].define(version: 1) do
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "phone", null: false
+    t.string "hometown", null: false
+    t.string "zipcode", null: false
+    t.datetime "created_at", null: false
+    t.datetime "last_update", null: false
+  end
+end
+
+# has_many trips
