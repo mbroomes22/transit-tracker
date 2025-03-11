6c312594d860de70c96e1e220dc159b8ee28faff
mbroomes22 <michelle_broomes@yahoo.com>
2025-03-11 16:54:40 -0400

    handle loading images

--- client/src/MbtaTrainMap.js
+++ client/src/MbtaTrainMap.js
@@ -1,15 +1,34 @@
+import React, {useState} from 'react';
 import map from './subway-map.jpg';
 import './stylesheets/App.css';
 // import LiveMap from './LiveMap.js';
 
 function MbtaTrainMap() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Here is the map
-        </p>
-        <img src={map} className="App-logo" alt="mbta logo" />
+        {isLoaded ? 
+        <div>
+          <p>
+            Here is the map
+          </p>
+        </div>
+         : 
+         <div className='placeholder'>
+          Loading Map...
+          </div>}
+         <img 
+        src={map} 
+        className="App-logo" 
+        alt="mbta logo" 
+        onLoad={handleImageLoad}
+        />
       </header>
     </div>
   );
