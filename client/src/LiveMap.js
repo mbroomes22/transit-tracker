837f289109ef70f4de7914c20b7fa087c2ba0d52
mbroomes22 <michelle_broomes@yahoo.com>
2025-03-18 13:51:16 -0400

    added vehicale tracking map

--- client/src/LiveMap.js
+++ client/src/LiveMap.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import TileLayer from 'ol/layer/Tile';
+import { RealtimeLayer as TrackerLayer } from 'mobility-toolbox-js/ol';
 import OSM from 'ol/source/OSM';
 import Map from 'ol/Map';
 import View from 'ol/View';
@@ -9,37 +10,63 @@ import Button from '@mui/material/Button';
 import FitExtent from 'react-spatial/components/FitExtent';
 import BasicMap from 'react-spatial/components/BasicMap';
 import Geolocation from 'react-spatial/components/Geolocation';
+import RouteSchedule from 'react-spatial/components/RouteSchedule';
 import 'ol/ol.css';
 
 // const extent = [-13884991, 2870341, -7455066, 6338219]; for USA
 // const extent = [-8100000, 4950000, -7900000, 5300000] // for MA
 const extent = [-7930000, 5190000, -7890000, 5230000] // for Boston, MA
 
-const map = new Map({
-  view: new View({
-    center: [-7910000, 5210000],
-    zoom: 4,
-  }),
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  controls: [],
+const trackerLayer = new TrackerLayer({
+  url: 'wss://api.geops.io/tracker-ws/v1/ws',
+  apiKey: process.env.REACT_APP_GEOPS_API_KEY,
 });
 
-const LiveMap = () => (
+const layers = [
+  new TileLayer({
+    source: new OSM(),
+  }),
+  trackerLayer,
+];
+
+const LiveMap = () => {
+  const [lineInfos, setLineInfos] = useState(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!mapRef.current) {
+      mapRef.current = new Map({
+        target: 'map',
+        view: new View({
+          center: [-7910000, 5210000],
+          zoom: 4,
+        }),
+        layers: layers,
+        controls: [],
+      });
+    }
+  }, []);
+
+  return (
   <ThemeProvider theme={geopsTheme}>
-    <div className="map-container">
-    <BasicMap map={map} tabIndex={0} className="basic-map" />
-    <Geolocation map={map} />
-    <FitExtent map={map} extent={extent}>
+    <div className="map-container" id="map">
+    <BasicMap map={mapRef.current} tabIndex={0} className="basic-map" />
+    <Geolocation map={mapRef.current} />
+    <FitExtent map={mapRef.current} extent={extent}>
       <Button>
         Fit to Boston
       </Button>
     </FitExtent>
+    <RouteSchedule
+          lineInfos={lineInfos}
+          trackerLayer={trackerLayer}
+          renderHeaderButtons={(routeIdentifier) => (
+            <Button onClick={() => setLineInfos(null)}>Clear</Button>
+          )}
+        />
     </div>
   </ThemeProvider>
 );
+};
 
 export default LiveMap;
---
+++ client/src/propTypes.js
@@ -0,0 +1,67 @@
+import PropTypes from "prop-types";
+
+const STATE_BOARDING = "BOARDING";
+const STATE_LEAVING = "LEAVING";
+const STATE_PENDING = "PENDING";
+const STATE_TIME_BASED = "TIME_BASED";
+const STATE_STOP_CANCELLED = "STOP_CANCELLED";
+const STATE_JOURNEY_CANCELLED = "JOURNEY_CANCELLED";
+
+const station = PropTypes.shape({
+  aimedArrivalTime: PropTypes.number, // time in milliseconds.
+  aimedDepartureTime: PropTypes.number, // time in milliseconds.
+  arrivalDelay: PropTypes.number, // time in milliseconds.
+  arrivalTime: PropTypes.number, // time in milliseconds with the delay included.
+  cancelled: PropTypes.bool,
+  coordinates: PropTypes.arrayOf(PropTypes.number),
+  departureDelay: PropTypes.number, // time in milliseconds.
+  departureTime: PropTypes.number, // time in milliseconds with the delay included
+  noDropOff: PropTypes.bool,
+  noPickUp: PropTypes.bool,
+  state: PropTypes.oneOf([
+    null,
+    STATE_BOARDING,
+    STATE_LEAVING,
+    STATE_PENDING,
+    STATE_TIME_BASED,
+    STATE_STOP_CANCELLED,
+    STATE_JOURNEY_CANCELLED,
+  ]),
+  stationId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  stationName: PropTypes.string,
+  wheelchairAccessible: PropTypes.bool,
+});
+
+const lineInfos = PropTypes.shape({
+  backgroundColor: PropTypes.string,
+  bicyclesAllowed: PropTypes.bool,
+  color: PropTypes.string,
+  destination: PropTypes.string,
+  feedsId: PropTypes.number,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  license: PropTypes.string,
+  licenseNote: PropTypes.string,
+  licenseUrl: PropTypes.string,
+  longName: PropTypes.string,
+  operatingInformations: PropTypes.object,
+  operator: PropTypes.string,
+  operatorTimeZone: PropTypes.string,
+  operatorUrl: PropTypes.string,
+  publisher: PropTypes.string,
+  publisherTimeZone: PropTypes.string,
+  publisherUrl: PropTypes.string,
+  realTime: PropTypes.number,
+  shortName: PropTypes.string,
+  stations: PropTypes.arrayOf(station),
+  vehicleType: PropTypes.number,
+  wheelchairAccessible: PropTypes.bool,
+});
+
+const propTypes = {
+  lineInfos,
+  STATE_BOARDING,
+  STATE_LEAVING,
+  station,
+};
+
+export default propTypes
