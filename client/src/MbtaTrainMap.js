8bb2b33270e527df3e1d39dbd21d95bc80be2b3b
mbroomes22 <michelle_broomes@yahoo.com>
2025-03-17 20:35:51 -0400

    displays static and live maps on train map page

--- client/src/MbtaTrainMap.js
+++ client/src/MbtaTrainMap.js
@@ -1,10 +1,15 @@
 import React, {useState} from 'react';
 import map from './subway-map.jpg';
 import './stylesheets/App.css';
-// import LiveMap from './LiveMap.js';
+import LiveMap from './LiveMap.js';
 
 function MbtaTrainMap() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [showLiveMap, setShowLiveMap] = useState(false);
+
+  const toggleMap = () => {
+    setShowLiveMap((prevShowLiveMap) => !prevShowLiveMap);
+  }
 
   const handleImageLoad = () => {
     setIsLoaded(true);
@@ -13,22 +18,31 @@ function MbtaTrainMap() {
   return (
     <div className="App">
       <header className="App-header">
-      {isLoaded ? 
-        <div>
-          <p>
-            Here is the map
-          </p>
-        </div>
-         : 
+        <button onClick={toggleMap}>
+          {showLiveMap ? 'Show Static Map' : 'Show Live Map'}
+        </button>
+        {showLiveMap ? (<LiveMap />) : (
+          <>
+        {isLoaded ? (
+          <div>
+            <p>
+              Here is the map
+            </p>
+          </div>
+          ) : (
          <div className='placeholder'>
           Loading Map...
-          </div>}
+          </div>
+        )}
          <img 
         src={map} 
         className="App-logo" 
         alt="mbta logo" 
-        onLoad={handleImageLoad} />
-        {/* <LiveMap /> */}
+        onLoad={handleImageLoad} 
+        />
+        </>
+        )
+      }
       </header>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './stylesheets/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'ol/ol.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
