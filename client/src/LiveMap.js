c265cc8f55a86410759805855088434cb3c4f1a1
mbroomes22 <michelle_broomes@yahoo.com>
2025-03-24 13:48:51 -0400

    removed erroring log line

--- client/src/LiveMap.js
+++ client/src/LiveMap.js
@@ -24,7 +24,7 @@ import VectorSource from 'ol/source/Vector';
 import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
 
 console.log("process.env.REACT_APP_GEOPS_API_KEY:", process.env.REACT_APP_GEOPS_API_KEY);
-console.log("ENV[REACT_APP_GEOPS_API_KEY]:", ENV["REACT_APP_GEOPS_API_KEY"]);
+
 
 
 const trackerLayer = new TrackerLayer({
