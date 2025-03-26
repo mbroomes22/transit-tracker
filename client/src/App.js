87959cf147a8dd96fe01baeb8346cef7069662bd
mbroomes22 <michelle_broomes@yahoo.com>
2025-03-26 12:05:43 -0400

    added trip planner

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import MbtaTrainMap from './MbtaTrainMap.js';
 import Welcome from './Welcome.js';
 import TrainNews from './components/TrainNews.js';
 import TripSchedule from './components/VehicleRoutes.js';
+import TripPlanner from './TripPlanner.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/maps" element={<MbtaTrainMap />} />
         <Route path="/news" element={<TrainNews />} />
         <Route path="/schedule" element={<TripSchedule />} />
+        <Route path="/trip-planner" element={<TripPlanner />} />
       </Routes>
     </Router>
   );
---
+++ client/src/TripPlanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TripPlanner() {
+
+    return(
+        <div>
+            <h1>Plan Your Trip</h1>
+        </div>
+    )
+}
+
+export default TripPlanner;
